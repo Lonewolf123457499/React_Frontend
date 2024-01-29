503b6e76ae15aed426cf8fddc80fc68db4f1ab82
vedant singh <93176679+Lonewolf123457499@users.noreply.github.com>
2024-01-29 17:21:17 +0530

    commited

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
+import React from "react"
+import  ReactDOM from "react-dom/client"
 const heading = React.createElement("h1", {
     id:"heading",
-    class:"header"
 
 }, "This is my first ReactApp")
 const root = ReactDOM.createRoot(document.getElementById("root"))
