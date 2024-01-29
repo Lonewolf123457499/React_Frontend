48398edf1617e5f51ddc6ca005133d84f36010b5
vedant singh <93176679+Lonewolf123457499@users.noreply.github.com>
2024-01-29 12:26:06 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id:"heading",
+    class:"header"
+
+}, "This is my first ReactApp")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
